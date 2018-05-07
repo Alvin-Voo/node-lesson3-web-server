ad32b6f617ca68f98e70f039a478437ec79b44b4
Alvin Voo <alvin.voo@gmail.com>
2018-05-07 15:37:21 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+app.use((req, res, next)=>{
+  //middleware mounted without a path will be executed for every request to the app.
+  let now = new Date().toString();
+
+  console.log(`${now}: ${req.method} ${req.url}`);
+
+  fs.appendFile('server.log', `${now}: ${req.method} ${req.url} \n`, (err)=>{
+    if(err) console.log('unable to append server log')
+  })
+  next();
+})
+
+// app.use((req, res, next)=>{//handy code to redirect all url to maintenance page, and not having to forward the next handler
+//   res.render('maintenance');
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{//hbs will look for helper first before looking to the arguments passed in
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (input)=>{
+  return input.toUpperCase();
+})
+
+app.get('/', (req, res)=>{
+  res.render('home',{
+    pageTitle: 'Home',
+    name: 'Alvin'
+  })
+});
+
+app.get('/about', (req,res)=>{
+  res.render('about',{
+    pageTitle: 'About'
+  });
+});
+
+app.get('/bad', (req,res)=>{
+  res.send('bad request');
+});
+
+app.listen(3000, ()=>console.log('listening at 3000'));
